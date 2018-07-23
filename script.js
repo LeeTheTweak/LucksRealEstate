6ed974c7b152b9d02fe087fca6b1d2fd29cb7ab1
LeeTheTweak <christopherleveston@outlook.com>
2018-07-22 20:16:00 -0500

    updated files with javascript and jquery

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+var tagLineEl = document.getElementById("tagLine");
+$(document).ready(function(){
+    $(tagLineEl).hide().delay().fadeIn(1500);
+});
+function changeTagLine() {
+    $(tagLineEl).hide().fadeIn(70);
+    tagLineEl.textContent = "Live the dream";
+}
+function changeTagLineBack() {
+    $(tagLineEl).hide().fadeIn(700);
+    tagLineEl.textContent = "Find your dream home";
+}
+tagLineEl.addEventListener("mouseover", changeTagLine,false);
+tagLineEl.addEventListener("mouseout", changeTagLineBack,false);
